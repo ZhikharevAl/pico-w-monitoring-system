d38dca42844dfa30ad631c25bb3ec502b6ac8f07
AlexeyZh <waltafunk@gmail.com>
2025-11-20 01:48:53 +0300

    chore: added config_example

---
+++ config_example.py
@@ -0,0 +1,10 @@
+WIFI_SSID = "Keenetic-9999"
+WIFI_PASSWORD = "wjdJld9Ht8"
+
+MQTT_SERVER = "192.168.1.51"
+MQTT_PORT = 1883
+MQTT_TOPIC = b"pico/metrics"
+CLIENT_ID = "pico-w-micropython"
+
+
+PUBLISH_INTERVAL = 10
